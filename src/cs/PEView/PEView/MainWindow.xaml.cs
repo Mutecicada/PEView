6e2afdc38f6989f14f095f62e2f0801bb1e7a03d
Mutecicada <mutecicada@naver.com>
2019-05-10 10:03:22 +0900

    C++ -> C#, WPF 기본적인 UI만

---
+++ src/cs/PEView/PEView/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+using System.Diagnostics;
+
+namespace PEView
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PEHeaders pe = new PEHeaders();
+
+            string strFileName = "";
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == true)
+            {
+                strFileName = ofd.FileName;
+            }
+
+            FileStream fp = new FileStream(strFileName, FileMode.Open, FileAccess.Read);
+            if (fp == null)
+            {
+                Console.WriteLine("FIle Open Error");
+            }
+
+            pe.dos_header = (PEHeaders._DOS_HEADER)pe.read_header(fp, typeof(PEHeaders._DOS_HEADER));
+
+
+            byte[] machine = new byte[2];
+            fp.Seek(pe.dos_header.e_lfanew + 4, SeekOrigin.Begin);
+            fp.Read(machine, 0, 2);
+
+            int numberofsections;
+            fp.Seek(pe.dos_header.e_lfanew, SeekOrigin.Begin);
+            if (BitConverter.ToInt16(machine, 0) == 0x014c)                 // 32bit
+            {
+                pe.nt_header32 = (PEHeaders._NT_HEADERS32)pe.read_header(fp, typeof(PEHeaders._NT_HEADERS32));
+                numberofsections = pe.nt_header32.FileHeader.NumberOfSections;
+            }
+            else                                                            // 64 bit
+            {
+                pe.nt_header64 = (PEHeaders._NT_HEADERS64)pe.read_header(fp, typeof(PEHeaders._NT_HEADERS64));
+                numberofsections = pe.nt_header64.FileHeader.NumberOfSections;
+            }
+
+            pe.section_headers = new PEHeaders._SECTION_HEADER[numberofsections];
+            for (int i = 0; i < numberofsections; i++)
+            {
+                pe.section_headers[i] = (PEHeaders._SECTION_HEADER)pe.read_header(fp, typeof(PEHeaders._SECTION_HEADER));
+            }
+
+            for (int i = 0; i < numberofsections; i++)
+            {
+                TreeViewItem section = new TreeViewItem();
+                section.Header = String.Join("", pe.section_headers[i].Name);
+                PETop.Items.Add(section);
+            }
+
+        }
+    }
+}
---
+++ src/cs/PEView/PEView/PEHeaders.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.InteropServices;
+
+
+namespace PEView
+{
+    class PEHeaders
+    {
+        public PEHeaders() { }
+
+        public object read_header(FileStream fp, Type type)
+        {
+            int size = Marshal.SizeOf(type);
+            byte[] data = new byte[size];
+            fp.Read(data, 0, size);
+
+            IntPtr buff = Marshal.AllocHGlobal(data.Length);        // 배열의 크기만큼 비관리 메모리 영역에 메모리를 할당한다.
+            Marshal.Copy(data, 0, buff, data.Length);               // 배열에 저장된 데이터를 위에서 할당한 메모리 영역에 복사한다.
+            object obj = Marshal.PtrToStructure(buff, type);        // 복사된 데이터를 구조체 객체로 변환한다.
+            Marshal.FreeHGlobal(buff);                              // 비관리 메모리 영역에 할당했던 메모리를 해제함
+
+            if (Marshal.SizeOf(obj) != data.Length)                 // (((PACKET_DATA)obj).TotalBytes != data.Length) // 구조체와 원래의 데이터의 크기 비교
+            {
+                return null;                                        // 크기가 다르면 null 리턴
+            }
+
+            return obj;                                             // 구조체 리턴
+        }
+
+
+        [StructLayout(LayoutKind.Sequential, Size = 1)]
+        public struct _DOS_HEADER  
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+            public char[] e_magic;                     // 2
+            ushort e_cblp;
+            ushort e_cp;
+            ushort e_crlc;
+            ushort e_cparhdr;
+            ushort e_minalloc;
+            ushort e_maxalloc;
+            ushort e_ss;
+            ushort e_sp;
+            ushort e_csum;
+            ushort e_ip;
+            ushort e_cs;
+            ushort e_lfarlc;
+            ushort e_ovno;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 4)]
+            ushort[] e_res;                        // 4
+            ushort e_oemid;
+            ushort e_oeminfo;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 10)]
+            ushort[] e_res2;                      // 10
+            public int e_lfanew;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 1)]
+        public struct _FILE_HEADER  
+        {
+            ushort Machine;
+            public ushort NumberOfSections;
+            uint TimeDateStamp;
+            uint PointerToSymbolTable;
+            uint NumberOfSymbols;
+            ushort SizeOfOptionalHeader;
+            ushort Characteristics;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 1)]
+        public struct _IMAGE_DATA_DIRECTORY  
+        {
+            uint VirtualAddress;
+            uint Size;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 1)]
+        public struct _OPTIONAL_HEADER32  
+        {
+            //
+            // Standard fields.
+            //
+            ushort Magic;
+            byte MajorLinkerVersion;
+            byte MinorLinkerVersion;
+            uint SizeOfCode;
+            uint SizeOfInitializedData;
+            uint SizeOfUninitializedData;
+            uint AddressOfEntryPoint;
+            uint BaseOfCode;
+            uint BaseOfData;
+
+            //
+            // NT additional fields.
+            //
+
+            uint ImageBase;
+            uint SectionAlignment;
+            uint FileAlignment;
+            ushort MajorOperatingSystemVersion;
+            ushort MinorOperatingSystemVersion;
+            ushort MajorImageVersion;
+            ushort MinorImageVersion;
+            ushort MajorSubsystemVersion;
+            ushort MinorSubsystemVersion;
+            uint Win32VersionValue;
+            uint SizeOfImage;
+            uint SizeOfHeaders;
+            uint CheckSum;
+            ushort Subsystem;
+            ushort DllCharacteristics;
+            uint SizeOfStackReserve;
+            uint SizeOfStackCommit;
+            uint SizeOfHeapReserve;
+            uint SizeOfHeapCommit;
+            uint LoaderFlags;
+            uint NumberOfRvaAndSizes;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+            _IMAGE_DATA_DIRECTORY[] DataDirectory;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 1)]
+        public struct _OPTIONAL_HEADER64  
+        {
+            ushort Magic;
+            byte MajorLinkerVersion;
+            byte MinorLinkerVersion;
+            uint SizeOfCode;
+            uint SizeOfInitializedData;
+            uint SizeOfUninitializedData;
+            uint AddressOfEntryPoint;
+            uint BaseOfCode;
+            ulong ImageBase;
+            uint SectionAlignment;
+            uint FileAlignment;
+            ushort MajorOperatingSystemVersion;
+            ushort MinorOperatingSystemVersion;
+            ushort MajorImageVersion;
+            ushort MinorImageVersion;
+            ushort MajorSubsystemVersion;
+            ushort MinorSubsystemVersion;
+            uint Win32VersionValue;
+            uint SizeOfImage;
+            uint SizeOfHeaders;
+            uint CheckSum;
+            ushort Subsystem;
+            ushort DllCharacteristics;
+            ulong SizeOfStackReserve;
+            ulong SizeOfStackCommit;
+            ulong SizeOfHeapReserve;
+            ulong SizeOfHeapCommit;
+            uint LoaderFlags;
+            uint NumberOfRvaAndSizes;
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+            _IMAGE_DATA_DIRECTORY[] DataDirectory;                  // 16
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 1)]
+        public struct _NT_HEADERS32  
+        {
+
+            public uint Signature;
+
+            public _FILE_HEADER FileHeader;
+
+            _OPTIONAL_HEADER32 OptionalHeader;
+
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 1)]
+        public struct _NT_HEADERS64  
+        {
+            uint Signature;
+            public _FILE_HEADER FileHeader;
+            _OPTIONAL_HEADER64 OptionalHeader;
+        }
+
+        [StructLayout(LayoutKind.Sequential, Size = 1)]
+        public struct _SECTION_HEADER  
+        {
+            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+            public char[] Name;
+
+            uint VirtualSize;
+
+            uint VirtualAddress;
+            uint SizeOfRawData;
+
+            uint PointerToRawData;
+            uint PointerToRelocations;
+            uint PointerToLinenumbers;
+
+            ushort NumberOfRelocations;
+            ushort NumberOfLinenumbers;
+
+            uint Characteristics;
+
+        }
+
+
+        public _DOS_HEADER dos_header;
+        public _NT_HEADERS32 nt_header32;
+        public _NT_HEADERS64 nt_header64;
+        public _SECTION_HEADER[] section_headers;
+    }
+}
+
